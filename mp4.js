4ac83ef86b55db46af2fc2806e32d74958faddf4
José Pedro Dias <jose.pedro.dias@gmail.com>
2015-01-19 03:44:25 +0000

    video with fake fs and autoplay

--- mp4.js
+++ mp4.js
@@ -1,18 +1,35 @@
 (function() {
 	'use strict';
 
+	var fsOn = function() {
+		var s = this.style;
+		s.left = 0;
+		s.top  = 0;
+		s.width  = '100%';
+		s.height = '100%';
+		s.backgroundColor = '#000';
+	};
+
+	var fsOff = function() {
+		var s = this.style;
+		s.left = '';
+		s.top  = '';
+		s.width  = '';
+		s.height = '';
+		s.backgroundColor = '';
+	};
+
+	var enrichVideoElement = function(v) {
+		v.fsOn  = fsOn;
+		v.fsOff = fsOff;
+	};
+
+
 	var v = document.createElement('video');
 	document.body.appendChild(v);
+	v.setAttribute('autoplay', '');
 	v.src = 'http://vjs.zencdn.net/v/oceans.mp4';
-
-	setTimeout(function() {
-		log('play!');
-		v.play();
-
-		setTimeout(function() {
-			log('pause!');
-			v.pause();
-		}, 1500);
-	}, 500);
+	enrichVideoElement(v);
+	window.v = v;
 
 })();
